67be58a89243b477d80bf98e4302418b6be235aa
Kimyen <kimyen0816@gmail.com>
2014-10-22 11:29:11 -0700

    add id to Log File

--- src/main/java/org/sagebionetworks/dashboard/dao/LogFileDao.java
+++ src/main/java/org/sagebionetworks/dashboard/dao/LogFileDao.java
@@ -2,7 +2,7 @@ package org.sagebionetworks.dashboard.dao;
 
 public interface LogFileDao {
 
-    void put(String filePath, int logType);
+    void put(String filePath, String id, int logType);
 
     void cleanup();
 
